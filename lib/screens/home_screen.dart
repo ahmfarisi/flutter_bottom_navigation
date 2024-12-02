6c16571899cd93dc1e0f511a71507cdc3b8f138e
ahmfarisi <ahmfarisi@gmail.com>
2024-12-02 10:23:40 +0700

    Create Screens and initial screen template in home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text('Ini adalah halaman home'),
+      ),
+    );
+  }
+}
